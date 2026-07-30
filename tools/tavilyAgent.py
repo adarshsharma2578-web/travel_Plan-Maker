f7e473228c3d9dc7c7d7cc3f64463f9e24e994b1
Adarsh <adarshsharma2578@gmail.com>
2026-07-30 19:26:57 +0530

    Initial commit

---
+++ tools/tavilyAgent.py
@@ -0,0 +1,16 @@
+import os
+
+from tavily import tavilyClient
+import dotenv
+tavilt = tavilyClient(api_key=os.environ.get("TAVILY_API_KEY"))
+user_query = input("what would you like to search on web")
+if user_query.strip():
+    print(f"\nSearching for:{user_query}")
+    response = tavily.search(query=user,max_result=3)
+
+    for i,result in enumerate(response['result'],1):
+        print(f"[{i}]{result[title]}")
+        print(f"URL:{result['url']}")
+        print(f"Snippet:{result['content']}\n"+"-"*40)
+else:
+    print("Search quesry is empty")
