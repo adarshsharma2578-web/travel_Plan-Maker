4f983f7a2549fe387229d8fbc74acb5b005f7262
Adarsh <adarshsharma2578@gmail.com>
2026-08-01 19:25:11 +0530

    Update llm.py

--- llm.py
+++ llm.py
@@ -1,16 +1,15 @@
-from tavily import TavilyClient
-import python_weather
+from openai import OpenAI
+from tools.tavilyAgent import TavilyAgent
+from tools.weather import get_weather
 import config
 
-from tavily import tavily
-from weather import weather
 
 class LLMManager:
-
-
-    self.tavily = tavily
-    self.weather = weather
-
+    def __init__(self):
+        self.tavily = TavilyAgent()
+        self.weather = get_weather
+        self.openai = OpenAI(api_key=config.OPENAI_API_KEY) if config.OPENAI_API_KEY else None
 
 
 llm = LLMManager()
+
